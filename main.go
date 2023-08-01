bdd25bc0fa018a764b5c08bdc19a344244c8589e
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-08-01 15:42:00 +0100

    1. web application basics

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// handler
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
 
 func main() {
-	fmt.Println("Hello World")
+	//initalise a new router/servemux which will map a url to a handler
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	log.Println("Starting Server on :4000")
+
+	//start a new web server
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
