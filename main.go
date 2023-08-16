40da097acdbccd1041fcbfa7b620bedc241d914e
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-08-16 16:43:02 +0100

    1. url query strings

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"fmt"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	 //ensure the URL path exactly matches "/" so home handler is only called on the homepage
     if r.URL.Path != "/" {
         http.NotFound(w, r)
 		return
@@ -15,13 +16,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	idFromUrl := r.URL.Query().Get("id")
+	if idFromUrl == "" {
+		http.Error(w, "no id entered", http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.Atoi(idFromUrl)
+	if err != nil || id < 1 {
+		http.Error(w, "invalid id", http.StatusNotFound)
+		return
+	}
+	
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		//lightweight helper function which takes a given message and status code, then calls the w.WriteHeader() and w.Write() methods behind-the-scenes.
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
