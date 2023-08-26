a7efdef0c0c59d116e053b37cebcaf99a8951968
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-08-26 13:33:39 +0100

    2. leveled logging - info and error logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,6 +13,12 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	//Parse reads in the CL flag value and assigns it to the addr variable. Must be called before using the addr variable.
 	flag.Parse()
+
+	//create a logger for writing information messages, which will write to std out
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	//create a logger for writing error messages, which will write to std err
+	//log.Lshortfile flag will include the file name and the line number
+	errLog:= log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	//initalise a new router/servemux which will map a url to a handler
 	mux := http.NewServeMux()
@@ -28,11 +35,11 @@ func main() {
 	//register the file server as the handler
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting Server on %s", *addr)
+	infoLog.Printf("Starting Server on %s", *addr)
 
 	//start a new web server
 	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
-		log.Fatal(err)
+		errLog.Fatal(err)
 	}
 }
