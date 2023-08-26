440c18ff301906ba7630dcdd85f97a010d4e3994
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-08-26 12:40:23 +0100

    2. managing config settings - moving network address to CLI

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	//add a command line flag for the network address
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	//Parse reads in the CL flag value and assigns it to the addr variable. Must be called before using the addr variable.
+	flag.Parse()
+
 	//initalise a new router/servemux which will map a url to a handler
 	mux := http.NewServeMux()
 
@@ -16,15 +23,15 @@ func main() {
 	// Create a file server which serves files out of the "./ui/static" directory
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	//the prefix "/static" is stripped, so the remaining part of the URL eg "/image.jpg" 
-	//is used to find the corresponding file in the "./ui/static" directory. 
+	//the prefix "/static" is stripped, so the remaining part of the URL eg "/image.jpg"
+	//is used to find the corresponding file in the "./ui/static" directory.
 	//register the file server as the handler
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting Server on :4000")
+	log.Printf("Starting Server on %s", *addr)
 
 	//start a new web server
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
