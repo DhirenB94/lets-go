7ae27416f03b90139c5892e706442ae2275b3606
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-08-28 16:04:08 +0100

    3. centralised error handling - report error from one step back in the stack trace

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,19 +5,20 @@ import (
 	"net/http"
 	"runtime/debug"
 )
-//serverError writes an error to the stack trace to the customLogger, then sends a generic 500 internal server error to the user
-func (app *application) severError (w http.ResponseWriter, err error) {
+
+// serverError writes an error to the stack trace to the customLogger, then sends a generic 500 internal server error to the user
+func (app *application) severError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Println(trace)
+	app.errorLog.Output(2, trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-//clientError sends a specific status code and corresponding desription to the user
-func (app *application) clientError(w http.ResponseWriter, status int)  {
+// clientError sends a specific status code and corresponding desription to the user
+func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
 
-//notFound is a wrapper of the clientErorr to send a 404 not found response to the user
-func (app *application) notFound(w http.ResponseWriter)  {
+// notFound is a wrapper of the clientErorr to send a 404 not found response to the user
+func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
