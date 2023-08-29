9b013012c62324fbf10955b1826fd0e21a99e8aa
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-08-29 19:01:51 +0100

    4. creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-//Define struct to hold application-wide dependencies 
+// Define struct to hold application-wide dependencies
 type application struct {
-	infoLog *log.Logger
+	infoLog  *log.Logger
 	errorLog *log.Logger
 }
 
 func main() {
 
-	//Comman line Flag for netowrok address
+	//Command line Flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	//Defenining a new CL flag for MySQL Data Source Name string
+	dsn := flag.String("dsn", "web:dhiren@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	//Custom Logger
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errLog:= log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	//Initialise a connection pool to the database
+	//The sql.Open doesnt actually create a connection, just initiialises a pool for future use
+	//so db.Ping method is used to create a connection and check for any errors
+	db, err := sql.Open("mysql", *dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		errLog.Fatal(err)
+	}
+	defer db.Close()
 
 	//Initialise a new instance of application contiaining the dependencies
 	app := &application{
@@ -34,15 +51,15 @@ func main() {
 
 	//Intialise a new Http server and set the address, handler and errorLog fields so that these are used instead of Go's default HTTP server settings
 	srv := http.Server{
-		Addr: *addr,
-		Handler: mux,
+		Addr:     *addr,
+		Handler:  mux,
 		ErrorLog: errLog,
 	}
 
 	infoLog.Printf("Starting Server on %s", *addr)
 
 	//call the listen and serve method on our new htpp server
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		errLog.Fatal(err)
 	}
