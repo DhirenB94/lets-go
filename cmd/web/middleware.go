6db81f983284692e847814c8ef1bcd4b87205371
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-09-05 17:07:34 +0100

    6. request logging - adding a middleware for logging requests

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -10,3 +10,11 @@ func secureHeaders(nextHandler http.Handler) http.Handler {
 		nextHandler.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) logRequest(nextHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s-%s, %s, %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		nextHandler.ServeHTTP(w, r)
+	})
+}
