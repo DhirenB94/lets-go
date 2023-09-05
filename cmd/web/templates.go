a9dae2b1274b0408425fe05aae6fd56c8f757c79
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-09-05 12:42:21 +0100

    5. custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	models "dhiren.brahmbhatt/snippetbox/pkg"
 )
@@ -12,6 +13,18 @@ type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
 }
+
+// humanDate returns a formatted easily readable string of the date
+func humanDate(t time.Time) string {
+    return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialise a template.FuncMap object and store it in a global variable. 
+// This is a string-keyed map which acts as a lookup between the names of of custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate" : humanDate, 
+}
+
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	//Initialise a new map to act as the cache
 	cache := map[string]*template.Template{}
@@ -26,8 +39,11 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		//extract the file name (e.g home.page.tmpl) from the full path
 		name := filepath.Base(page)
 
-		//parse the page template file into a template set
-		ts, err := template.ParseFiles(page)
+        // The template.FuncMap must be registered with the template set before we call the ParseFiles() method. 
+		// This means we have to use template.New to  create an empty template set,
+		// Then use the Funcs() method to register the template.FuncMap, 
+		// Then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
