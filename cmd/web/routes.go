a1727bf988d9ece3fe255a6a6641cda4ab68c063
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-09-09 15:46:45 +0100

    7. implementing RESTful routes - using bmizerany/pat

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,24 +3,26 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
 	//Initalise a new router/servemux
-	mux := http.NewServeMux()
+	mux := pat.New()
 
 	//create a standard middleware chain
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
 	//Routes
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
 
 	//File Server & register it as a handler
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
 	return standardMiddleware.Then(mux)
 }
