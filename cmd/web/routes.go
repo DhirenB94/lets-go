e59051322e41573623fcd943a4778962f520868c
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-09-13 11:45:57 +0100

    9. setting up the session manager - wrap dynamic routes with session middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,11 +14,18 @@ func (app *application) routes() http.Handler {
 	//create a standard middleware chain
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
+	//For the sessions to work, we need to wrap our application routes with the middleware provided by the Session.Enable() method. 
+	//This middleware loads and saves session data to and from the session cookie with every HTTP request and response as appropriate.
+	//We only need this middleware to act on our dynamic routes, the static route onlky serves static files and so does not need stateful behaviour
+	//create a dynamic middleware chain
+	dynamicMiddleware := alice.New(app.session.Enable)
+
 	//Routes
-	mux.Get("/", http.HandlerFunc(app.home))
-	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
-	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
-	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
+	//Uodate the routes using the dynamicmiddlewarechain
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+	mux.Get("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
+	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 
 	//File Server & register it as a handler
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
