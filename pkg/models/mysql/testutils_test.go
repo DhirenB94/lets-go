1afd6392d6f32ce5378561b551777afe44116ff8
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-09-18 14:47:37 +0100

    13. Integration testing - test database setup and teardown

---
+++ pkg/models/mysql/testutils_test.go
@@ -0,0 +1,42 @@
+package mysql
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
+
+func newTestDB(t *testing.T) (*sql.DB, func()) {
+	// Establish a sql.DB connection pool for our test database.
+	// We need to use the `multiStatements=true` parameter in our DSN.
+	// This allows multuplue SQL statements in one db.Exec() call
+	db, err := sql.Open("mysql", "web:dhiren@/snippetbox?parseTime=true&multiStatements=true")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//Read the setup SQL script file
+	script, err := os.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Execute the statements
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Return the connection pool and an anonymous function which reads anD executes the teardown script, and closes the connection pool. 
+	return db, func() {
+		script, err := os.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+		db.Close()
+	}
+}
