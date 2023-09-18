f04892a795c0120123d20dbeb3513e0db967e267
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2023-09-18 16:04:00 +0100

    13. Integration testing - testing the UserModel.Get method

--- pkg/models/mysql/testutils_test.go
+++ pkg/models/mysql/testutils_test.go
@@ -10,7 +10,7 @@ func newTestDB(t *testing.T) (*sql.DB, func()) {
 	// Establish a sql.DB connection pool for our test database.
 	// We need to use the `multiStatements=true` parameter in our DSN.
 	// This allows multuplue SQL statements in one db.Exec() call
-	db, err := sql.Open("mysql", "web:dhiren@/snippetbox?parseTime=true&multiStatements=true")
+	db, err := sql.Open("mysql", "test_web:pass@/test_snippetbox?parseTime=true&multiStatements=true")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -27,7 +27,7 @@ func newTestDB(t *testing.T) (*sql.DB, func()) {
 		t.Fatal(err)
 	}
 
-	// Return the connection pool and an anonymous function which reads anD executes the teardown script, and closes the connection pool. 
+	// Return the connection pool and an anonymous function which reads anD executes the teardown script, and closes the connection pool.
 	return db, func() {
 		script, err := os.ReadFile("./testdata/teardown.sql")
 		if err != nil {
